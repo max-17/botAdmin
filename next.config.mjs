11f5adde77f9e4949094b1edebe6f1efe6fa247e
max-17 <max.1724dn@gmail.com>
2025-04-17 17:37:29 +0500

    vercel

--- next.config.mjs
+++ next.config.mjs
@@ -17,23 +17,4 @@ const nextConfig = {
   },
 }
 
-if (userConfig) {
-  // ESM imports will have a "default" property
-  const config = userConfig.default || userConfig
-
-  for (const key in config) {
-    if (
-      typeof nextConfig[key] === 'object' &&
-      !Array.isArray(nextConfig[key])
-    ) {
-      nextConfig[key] = {
-        ...nextConfig[key],
-        ...config[key],
-      }
-    } else {
-      nextConfig[key] = config[key]
-    }
-  }
-}
-
 export default nextConfig
