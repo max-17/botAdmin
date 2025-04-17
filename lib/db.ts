2d92825635958005db3cfe6f43994e84a2289ba1
max-17 <max.1724dn@gmail.com>
2025-04-17 18:30:04 +0500

    vercel

--- lib/db.ts
+++ lib/db.ts
@@ -1,3 +1,11 @@
-import { PrismaClient } from "@/prisma/generated/prisma/client";
+import { PrismaClient } from "@/prisma/generated/prisma";
 
-export const db = new PrismaClient();
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const db = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+
+export default db;
